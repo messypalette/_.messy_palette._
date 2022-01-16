72f3c4f551c587ce32dd929aba484e0656887d94
messypalette <97799478+messypalette@users.noreply.github.com>
2022-01-16 13:29:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function page2(){
+    window.location ="index2.html";
+}
+function page1(){
+    window.location ="index.html";
+}
+function page3(){
+    window.location ="index3.html";
+}
+function puru(){
+    window.location ="https://www.instagram.com/puruagarwal4/";
+}
+function messy(){
+    window.location="https://www.instagram.com/_.messy_palette._/";
+}
